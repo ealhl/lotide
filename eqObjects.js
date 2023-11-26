56c85d6bceae648d71c1eaf4846a04ab3fb68c00
ealhl <yayt.tuk@gmail.com>
2023-11-26 10:44:44 -0800

    update code after Ai feedback array is array check object 1 and 2

--- eqObjects.js
+++ eqObjects.js
@@ -27,7 +27,7 @@ const eqObjects = function (object1, object2) {
   }
 
   for (let key of Object.keys(object1)) {
-    if (Array.isArray(object1[key])) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
