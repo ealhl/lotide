14cdaf9029ea4d223a3e01af468279c3ad2e8ced
ealhl <yayt.tuk@gmail.com>
2023-11-26 10:38:58 -0800

    update eqObjects final

--- eqObjects.js
+++ eqObjects.js
@@ -18,34 +18,38 @@ const eqArrays = function (arr, arr2) {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function (object1, object2) {
-  if (eqArrays(Object.keys(object1).sort(), Object.keys(object2).sort())) {
-    for (let key in object1) {
-      //check if key is array
-      if (Array.isArray(object1[key])) {
-        if (!eqArrays(object1[key], object2[key])) {
-          return false;
-        }
-      } else {
-        if (object1[key] !== object2[key]) {
-          return false;
-        }
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+
+  for (let key of Object.keys(object1)) {
+    if (Array.isArray(object1[key])) {
+      console.log(object1[key]);
+      console.log(object2[key]);
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
       }
     }
-    return true;
   }
-  return false;
+  return true;
 };
 
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject = { size: "medium", color: "red" };
-eqObjects(shirtObject, anotherShirtObject); // => true
-//We need to use that return value in combination with assertEquals to test if the function is working correctly.
-// assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
-
 const longSleeveShirtObject = {
   size: "medium",
   color: "red",
   sleeveLength: "long",
 };
+eqObjects(shirtObject, anotherShirtObject); // => true
+
+//We need to use that return value in combination with assertEquals to test if the function is working correctly.
+
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
+
 eqObjects(shirtObject, longSleeveShirtObject); // => false
 assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
