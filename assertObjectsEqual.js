d031502706be86d3730f1eaed87519102360889a
ealhl <yayt.tuk@gmail.com>
2023-11-26 10:49:28 -0800

    udpdate assertObjectEqual

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -2,35 +2,38 @@ const eqArrays = function (arr, arr2) {
   if (arr.length !== arr2.length) {
     return false;
   }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 const eqObjects = function (object1, object2) {
-  if (eqArrays(Object.keys(object1).sort(), Object.keys(object2).sort())) {
-    for (let key in object1) {
-      //check if key is array
-      console.log("inside the loop");
-      if (Array.isArray(object1[key])) {
-        if (!eqArrays(object1[key], object2[key])) {
-          console.log("array key not same ", object1[key],object2[key]);
-          return false;
-        }
-      } else {
-        if (object1[key] !== object2[key]) {
-          console.log("key not same", object1[key],object2[key]);
-          return false;
-        }
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+
+  for (let key of Object.keys(object1)) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
       }
     }
-    return true;
   }
-  return false;
+  return true;
 };
-
 // FUNCTION IMPLEMENTATION
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect; // <= add this line
-  console.log(`Example actual: ${inspect(actual)}`);
-  console.log(`Example expected: ${inspect(expected)}`);
+  // console.log(`Example actual: ${inspect(actual)}`);
+  // console.log(`Example expected: ${inspect(expected)}`);
   console.log(`Two objects are ${eqObjects(inspect(actual), inspect(actual)) ? 'equal' : 'not equal'}:`, inspect(actual), `and `,inspect(expected));
 
 
--- eqArrays.js
+++ eqArrays.js
@@ -1,6 +1,10 @@
 
 const assertEqual = function (actual, expected) {
-  console.assert(actual === expected);
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
 
 const eqArrays = function (arr, arr2) {
