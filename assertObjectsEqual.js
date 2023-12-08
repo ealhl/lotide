eb1dac6d0e27a2aee7ab8bfa870d41603e156b19
ealhl <yayt.tuk@gmail.com>
2023-12-07 16:43:23 -0800

    refactor test

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,34 +1,6 @@
-const eqArrays = function (arr, arr2) {
-  if (arr.length !== arr2.length) {
-    return false;
-  }
+const eqArrays = require("./eqArrays");
+const eqObjects = require("./eqObjects");
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const eqObjects = function (object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
-  }
-
-  for (let key of Object.keys(object1)) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!eqArrays(object1[key], object2[key])) {
-        return false;
-      }
-    } else {
-      if (object1[key] !== object2[key]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
 // FUNCTION IMPLEMENTATION
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect; // <= add this line
@@ -49,3 +21,5 @@ const longSleeveShirtObject = {
 };
 
 assertObjectsEqual(shirtObject, anotherShirtObject);
+
+module.exports = assertObjectsEqual;
